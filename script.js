6ecfe3ab87d55e58ca8c4b9d4078310fb19ed9b6
neptoon12 <58913578+neptoon12@users.noreply.github.com>
2020-05-12 17:26:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,210 @@
+
+// NUMBERS
+
+function n1(){
+var answer = document.getElementById('n1')
+if (answer.value == 1)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function n2(){
+var answer = document.getElementById('n2')
+if (answer.value == 2)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function n3(){
+var answer = document.getElementById('n3')
+if (answer.value == 3)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function n4(){
+var answer = document.getElementById('n4')
+if (answer.value == 4 )
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+// NUMBER ORDER
+
+function o1(){
+var answer = document.getElementById('o1')
+if (answer.value == 1)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function o2(){
+var answer = document.getElementById('o2')
+if (answer.value == 4)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function o3(){
+var answer = document.getElementById('o3')
+if (answer.value == 4)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function o4(){
+var answer = document.getElementById('o4')
+if (answer.value == 2)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+
+// LESS_GREATER
+
+
+function l1(){
+var answer = document.getElementById('l1')
+if (answer.value == 5)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function l2(){
+var answer = document.getElementById('l2')
+if (answer.value == 3)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function l3(){
+var answer = document.getElementById('l3')
+if (answer.value == 1)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function l4(){
+var answer = document.getElementById('l4')
+if (answer.value == 2)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+
+// SHAPES
+
+function h1(){
+var answer = document.getElementById('h1')
+if (answer.value == 0)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function h2(){
+var answer = document.getElementById('h2')
+if (answer.value == 3)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function h3(){
+var answer = document.getElementById('h3')
+if (answer.value == 4)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function h4(){
+var answer = document.getElementById('h4')
+if (answer.value == 5)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+
+// Addition
+
+function a1(){
+var answer = document.getElementById('a1')
+if (answer.value == 2)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function a2(){
+var answer = document.getElementById('a2')
+if (answer.value == 3)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function a3(){
+var answer = document.getElementById('a3')
+if (answer.value == 4)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function a4(){
+var answer = document.getElementById('a4')
+if (answer.value == 4)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+// Subtraction
+
+
+function a1(){
+var answer = document.getElementById('a1')
+if (answer.value == 1)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function s2(){
+var answer = document.getElementById('s2')
+if (answer.value == 1)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function s3(){
+var answer = document.getElementById('s3')
+if (answer.value == 2)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
+function s4(){
+var answer = document.getElementById('s4')
+if (answer.value == 4)
+alert("Good Job!");
+else
+alert("Incorrect. Try Again");
+}
+
